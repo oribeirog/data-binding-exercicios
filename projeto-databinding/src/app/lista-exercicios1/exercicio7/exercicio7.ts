9d301cf720d135cdea5848d2dd8f238484899736
Guilherme Ribeiro <guilhermeroliveira04@gmail.com>
2026-09-15 15:31:40 +0000

    Exercicio7

---
+++ projeto-databinding/src/app/lista-exercicios1/exercicio7/exercicio7.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicio7',
+  standalone: false,
+  styleUrl: './exercicio7.scss',
+  templateUrl: './exercicio7.html',
+})
+export class Exercicio7 {
+  texto: string = '';
+}
